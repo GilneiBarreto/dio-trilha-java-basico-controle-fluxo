58e8c0d3a4f5c600f9cd7e34b5496a1fa254be38
Gilnei Barreto <gilneibarretos@gmail.com>
2024-05-02 01:12:59 -0300

    Atualizando a classe Contador

--- src/Contador.java
+++ src/Contador.java
@@ -21,7 +21,7 @@ public class Contador {
            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
 		int contagem = parametroDois - parametroUm;
         for(int i = 1; i <= contagem; i++){
-            System.out.println("Imprimeindo o número " + i);
+            System.out.println("Imprimindo o número " + i);
         }
 	}
 }
